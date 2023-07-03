5ad983e9f81c0e6e125d40e981b2b5c0d11e7f27
zahnna <nancytsaan@mail.ru>
2023-07-04 00:22:39 +0200

    Ca,era and CharacterMovements

---
+++ Assets/Scripts/CharacterControll.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CharacterControll : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float jumpPower = 5.0f;
+    
+    private Rigidbody2D _playerRigidbody;
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        if (_playerRigidbody == null)
+        {
+            Debug.LogError("Player is missing a Rigidbody2D component");
+        }
+    }
+    private void Update()
+    {
+        MovePlayer();
+
+        if (Input.GetButton("Jump"))
+            Jump();
+    }
+    private void MovePlayer()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
+    }
+    private void Jump() => _playerRigidbody.velocity = new Vector2( 0, jumpPower);
+
+}
