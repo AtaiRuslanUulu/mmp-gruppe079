e087a5e4f364d2b4dbef1a73c7888fbfa776be28
zahnna <nancytsaan@mail.ru>
2023-07-16 18:29:40 +0200

    Sounds for jumps, coins and the bg noize

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private AudioSource collectionSoundEffect;
     public int value;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Player")){
             Destroy(gameObject);
             CoinsCounter.instance.IncreaseCoins(value);
+            collectionSoundEffect.Play();
         }
     }
 }
