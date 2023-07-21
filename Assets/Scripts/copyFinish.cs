caa51c64f537fb0f3d891e237d09a029d41c0082
zahnna <nancytsaan@mail.ru>
2023-07-21 17:45:40 +0200

    final

---
+++ Assets/Scripts/copyFinish.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyFinish : MonoBehaviour
+{
+    private AudioSource finishSound;
+    public GameObject BackToMainMenuButton;
+    public GameObject FinishText;
+
+    void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private  void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" || collision.gameObject.CompareTag("Player"))
+        {
+            finishSound.Play();
+            BackToMainMenuButton.SetActive(true);
+            FinishText.SetActive(true);
+        }
+    }
+
+}
